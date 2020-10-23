b8acebcf2615409081fd640efe1692ede1e8b2a5
Alexei Danchenkov <alexeid@scientology.net>
2020-10-22 18:11:08 -0700

    slight change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "hello, world!"
+    render inline: "<h1>Hello, world!</h1><p>The time is now: <%= Time.now %></p>"
   end
 end
